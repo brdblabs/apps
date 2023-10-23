9b6765306df660facf5b2329fdfbc72b20c64769
brdblabs <96331019+brdblabs@users.noreply.github.com>
2023-10-23 15:55:50 +0400

    Update image_classifier.py

--- image_classifier.py
+++ image_classifier.py
@@ -1,10 +1,3 @@
-# Create Steamlit app
-import streamlit as st
-import subprocess
-
-packages = str(subprocess.run('pip list', capture_output=True))
-st.markdown(packages.replace('\\r\\n', '  \\\n'))
-
 def main():
   import tensorflow as tf
   import streamlit as st
