ffa924c8e2510708f17cc71def2681e5694f3574
brdblabs <96331019+brdblabs@users.noreply.github.com>
2023-10-23 15:16:11 +0400

    Update image_classifier.py

--- image_classifier.py
+++ image_classifier.py
@@ -1,5 +1,16 @@
 # Create Steamlit app
 import streamlit as st
+from PIL import Image
+import numpy as np
+import time
+
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
 def main():
   st.title('Cifar10 Web Classifier')
   st.write("Upload any image that you think fits into one of the classes and see if the prediction is correct.")
@@ -9,11 +20,32 @@ def main():
     image = Image.open(file)
     st.image(image, use_column_width=True)
 
-    resized_image((32, 32))
+    resized_image = image.resize((32, 32))
     img_array = np.array(resized_image) / 255
     img_array = img_array.reshape((1, 32, 32, 3))
 
-    model = tf.keras.models.load_model('cifar10_model.h5')
+    # Progress bar while model is running
+    st.write('Please standby while your image is analyzed...')
+
+    # Preprocessing
+    (X_train, y_train), (X_val, y_val) = cifar10.load_data()
+    X_train = X_train / 255
+    X_val = X_val /255
+
+    y_train = to_categorical(y_train, 10)
+    y_val = to_categorical(y_val, 10)
+
+    # Create model
+    model = Sequential([
+        Flatten(input_shape=(32, 32, 3)),
+        Dense(1000, activation='relu'),
+        Dense(1000, activation='relu'),
+        Dense(1000, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, y_train, batch_size=64, epochs=5, validation_data=(X_val, y_val))
 
     predictions = model.predict(img_array)
     cifar10_classes = ['airplane', 'automobile', 'bird', 'cat', 'dog', 'deer', 'frog', 'horse', 'ship', 'truck']
