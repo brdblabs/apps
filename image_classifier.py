8b432acce5ecbc847d66329059f8074b163b8b25
brdblabs <96331019+brdblabs@users.noreply.github.com>
2023-10-24 16:45:05 +0400

    Update image_classifier.py

--- image_classifier.py
+++ image_classifier.py
@@ -25,29 +25,8 @@ def main():
     # Progress bar while model is running
     st.write('Please standby while your image is analyzed...')
 
-    # Preprocessing
-    (X_train, y_train), (X_val, y_val) = cifar10.load_data()
-    X_train = X_train / 255
-    X_val = X_val /255
-
-    y_train = to_categorical(y_train, 10)
-    y_val = to_categorical(y_val, 10)
-
-    # Load model instead (upload saved model to github - ? how?)
-    # loaded_full_model = load_model('drive/My Drive/Data/models/20200131-03111580440309-all-images-Adam.h5')
-    # https://medium.com/linkit-intecs/how-to-upload-large-files-to-github-repository-2b1e03723d2
-    
-    # Create model
-    model = Sequential([
-        Flatten(input_shape=(32, 32, 3)),
-        Dense(1000, activation='relu'),
-        Dense(1000, activation='relu'),
-        Dense(1000, activation='relu'),
-        Dense(10, activation='softmax')
-    ])
-
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X_train, y_train, batch_size=64, epochs=5, validation_data=(X_val, y_val))
+    # Load saved model
+    model = tf.keras.models.load_model('cifar10_model.h5')
 
     predictions = model.predict(img_array)
     cifar10_classes = ['airplane', 'automobile', 'bird', 'cat', 'dog', 'deer', 'frog', 'horse', 'ship', 'truck']
