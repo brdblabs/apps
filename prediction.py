7f381c9c147c7df46510ef50e27ae95ed2c2b9d5
brdblabs <96331019+brdblabs@users.noreply.github.com>
2023-10-25 08:34:50 +0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
-def predict(data):
+def predict(img_array):
   model = tf.keras.models.load_model('cifar10_model.h5')
+
+
+
+
+  
     predictions = model.predict(img_array)
     cifar10_classes = ['airplane', 'automobile', 'bird', 'cat', 'dog', 'deer', 'frog', 'horse', 'ship', 'truck']
